258c97822120e14d0d9fa7b8c5c2eb7aa0a63e60
MYRI Deploy <deploy@myri.com>
2026-07-11 03:20:21 +0000

    DIAGNOSTIC: move debug box to bottom-left to test if it was covering the hamburger button

--- components/Nav.js
+++ components/Nav.js
@@ -68,7 +68,7 @@ export default function Nav() {
           </div>
         </Link>
 
-        <div style={{ position: "fixed", top: 4, right: 4, background: "red", color: "white", padding: "4px 8px", fontSize: 10, zIndex: 9999 }}>
+        <div style={{ position: "fixed", bottom: 4, left: 4, background: "red", color: "white", padding: "4px 8px", fontSize: 10, zIndex: 9999 }}>
           isMobile: {String(isMobile)}
         </div>
 
