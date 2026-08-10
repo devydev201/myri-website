cf0ef74f24cb88d2545b5ecf04628df95fb21fc6
devydev201 <charlesdreyes2011@gmail.com>
2026-08-10 10:59:00 -0400

    Add Privacy Policy link to Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -23,6 +23,7 @@ const COLS = [
       { label: "Pricing", href: "/pricing" },
       { label: "Contact Us", href: "/contact" },
       { label: "Free Billing Audit", href: "/contact" },
+      { label: "Privacy Policy", href: "/privacy-policy" },
     ],
   },
   {
